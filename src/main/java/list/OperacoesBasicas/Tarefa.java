996091620a397afdd55f1a3ea8b82c5cf4e56bf7
Natalia Nero <nataliasilva10@gmail.com>
2023-08-19 20:41:05 -0300

    Resolvendo desafio ListaTarefa
    Signed-off-by: Natalia Nero <nataliasilva10@gmail.com>

---
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,21 @@
+package main.java.list.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
+
+
